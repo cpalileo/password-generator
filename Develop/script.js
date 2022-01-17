cf82387ecdd65e726c9cc2256cfa700d742329fb
CPalileo <cpalileo@gmail.com>
2022-01-16 22:46:09 -0800

    logic added

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,21 @@
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var charArr = [];
-var passArr = [];
-var lowerCharacter;
-var upperCharacter;
-var numericCharacter;
-var specialCharacter;
-var length;
+const lowerChar = "abcdefghijklmnopqrstuvwxyz";
+const upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numericChar = "0123456789";
+const specialChar = "!@,#$%&*{}[]/\\+=";
+
+welcome();
 
 // Welcome screen
 function welcome() {
   window.alert("Welcome to your personal password generator.");
   selectLength();
 }
-
-welcome();
 
 // Prompt user for length and character types
 function selectLength() {
@@ -24,109 +24,56 @@ function selectLength() {
   );
   if (length >= 8 && length <= 128) {
     result = Number(length);
-    selectCharacters();
+    selectOptions(result);
   } else {
     window.alert("Sorry friend, please choose a value between 8 - 128");
     selectLength();
   }
 }
 
-function selectCharacters() {
-  lowerCharacter = window.confirm("Use lower case characters?");
-  upperCharacter = window.confirm("Use upper case characters?");
-  numericCharacter = window.confirm("Use numeric characters?");
-  specialCharacter = window.confirm("Use special characters?");
-  if (
-    !lowerCharacter &&
-    !upperCharacter &&
-    !numericCharacter &&
-    !specialCharacter
-  ) {
+function selectOptions(length) {
+  lowerOption = window.confirm("Use lower case characters?");
+  upperOption = window.confirm("Use upper case characters?");
+  numericOption = window.confirm("Use numeric characters?");
+  specialOption = window.confirm("Use special characters?");
+  if (!lowerOption && !upperOption && !numericOption && !specialOption) {
     window.alert(
       "Pardon me but you must select at least one type of character."
     );
-    selectCharacters();
+    selectOptions(length);
   } else {
-    randomNumber();
-  }
+    var possibleCharacters = "";
 
-  // Generate the random characters
+    if (lowerOption) {
+      possibleCharacters = possibleCharacters + lowerChar;
+    }
+    if (upperOption) {
+      possibleCharacters = possibleCharacters + upperChar;
+    }
+    if (numericOption) {
+      possibleCharacters = possibleCharacters + numericChar;
+    }
+    if (specialOption) {
+      possibleCharacters = possibleCharacters + specialChar;
+    }
 
-  function randomNumber(min, max) {
-    let range = max - min + 1;
-    return Math.floor(Math.random() * range) + min;
-  }
-  let values = Array.from({ length: length }, () => randomNumber(0, 9));
+    var password = "";
 
-  function randomLower(min, max) {
-    range = max - min + 1;
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-  }
-  values = Array.from({ length: length }, () => randomLower());
+    for (var i = 0; i < length; i++) {
+      var characterIndex = Math.floor(
+        Math.random() * possibleCharacters.length
+      );
 
-  function randomUpper(min, max) {
-    range = max - min + 1;
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-  }
-  values = Array.from({ length: length }, () => randomUpper());
+      // charAt treats string as an array
+      var randomCharacter = possibleCharacters.charAt(characterIndex);
 
-  function randomSpecial(min, max) {
-    range = max - min + 1;
-    return String.fromCharCode(Math.floor(Math.random() * 14 + 33));
-  }
-  values = Array.from({ length: length }, () => randomSpecial());
-
-  //excuse the formatting like this console log below. The extension I installed is a little off
-  console.log(
-    "Boolean values from character prompts" + length,
-    lowerCharacter,
-    upperCharacter,
-    numericCharacter,
-    specialCharacter
-  );
-}
-
-// FROM ADVICE
-//array that will contain all characters the user selects
-
-// var charactersForPassword = [
-//   randomLower ? [randomLower] : [],
-//   randomUpper ? [randomUpper] : [],
-//   randomNumber ? [randomNumber] : [],
-//   randomSpecial ? [randomSpecial] : [],
-// ];
-
-// console.log(charactersForPassword);
-
-// //if user selected special Characters
-// charactersForPassword.concat(specialCharactersArray);
-
-// // if user selected numbers
-// charactersForPassword.concat(numbersArray);
-
-// //if user selected lower
-// charactersForPassword.concat(lowerArray);
-
-// //if user selected upper
-// charactersForPassword.concat(upperCharactersArray);
-
-function randomizedChars() {
-  if (lowerCharacter) {
+      // takes password var adds the randomCharacter and saves it to the exiting password variable
+      password = password + randomCharacter;
+    }
+    // console.log(password);
+    // console.log(possibleCharacters.charAt(characterIndex));
   }
 }
-
-// //declare password string variable that will hold your password
-// var passwordString = "";
-
-// //loop the number of times that user wants their password length
-// for (let i = 0; i < userSelectedPasswordLength; i++) {
-//   var randomCharcter = (passwordString += randomCharcter); //GEt a random character from charactersForPassword
-// }
-
-// return passwordString;
-password = "passwordString";
-// //END FROM ADVICE
-
 // Generates password
 function generatePassword() {
   var passwordCreated = password;
@@ -140,6 +87,3 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
